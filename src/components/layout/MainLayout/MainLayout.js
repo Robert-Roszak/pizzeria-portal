3e49864eca52f7089da6db174612ff6ee17ad984
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2021-10-12 19:56:33 +0200

    Add waiter view

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { AppBar, Toolbar, Container } from '@mui/material';
+
 import PageNav from '../PageNav/PageNav';
 
 const MainLayout = ({children}) => {
   return (
-    <div>
-      <PageNav />
-      {children}
+    <div className="MainLayout">
+      <AppBar>
+        <Container maxWidth='lg'>
+          <Toolbar disableGutters>
+            <PageNav />
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Container maxWidth='lg'>
+        <Toolbar />
+        {children}
+      </Container>
     </div>
   );
 };
