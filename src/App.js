0e830d73a21a0561a52348697c0a57c3170571f7
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2021-10-14 19:21:14 +0200

    Revert "Add waiter view"
    This reverts commit 3e49864eca52f7089da6db174612ff6ee17ad984.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import { BrowserRouter } from 'react-router-dom';
 import { Switch, Route } from 'react-router';
-import { createTheme, ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 
 import Login from './components/views/Login/Login';
 import Kitchen from './components/views/Kitchen/Kitchen';
@@ -16,35 +15,24 @@ import NewOrder from './components/views/NewOrder/NewOrder';
 import Order from './components/views/Order/Order';
 import Homepage from './components/views/Homepage/Homepage';
 
-const theme = createTheme({
-  palette: {
-    primary: { main: '#2B4C6F' },
-    //secondary: { main: '#11cb5f' },
-  },
-});
-
 function App() {
   return (
     <BrowserRouter>
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <MainLayout>
-            <Switch>
-              <Route exact path={process.env.PUBLIC_URL + '/'} component={Homepage} />
-              <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-              <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component={NewBookingTable} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={BookedTable} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables/event/new'} component={NewBookingEvent} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables/event/:id'} component={BookedEvent} />
-              <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
-              <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={NewOrder} />
-              <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={Order} />
-            </Switch>
-          </MainLayout>
-        </ThemeProvider>
-      </StyledEngineProvider>
+      <MainLayout>
+        <Switch>
+          <Route exact path={process.env.PUBLIC_URL + '/'} component={Homepage} />
+          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component={NewBookingTable} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={BookedTable} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/event/new'} component={NewBookingEvent} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/event/:id'} component={BookedEvent} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={NewOrder} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={Order} />
+        </Switch>
+      </MainLayout>
     </BrowserRouter>
   );
 }
--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { AppBar, Toolbar, Container } from '@mui/material';
-
 import PageNav from '../PageNav/PageNav';
 
 const MainLayout = ({children}) => {
   return (
-    <div className="MainLayout">
-      <AppBar>
-        <Container maxWidth='lg'>
-          <Toolbar disableGutters>
-            <PageNav />
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <Container maxWidth='lg'>
-        <Toolbar />
-        {children}
-      </Container>
+    <div>
+      <PageNav />
+      {children}
     </div>
   );
 };
